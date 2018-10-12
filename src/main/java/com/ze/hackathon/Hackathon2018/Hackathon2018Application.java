08deb6f9ee47124a2f6e5ae004ab37067f725166
rexfound <rexfound@gmail.com>
2018-10-12 14:23:03 -0700

    init

---
+++ src/main/java/com/ze/hackathon/Hackathon2018/Hackathon2018Application.java
@@ -0,0 +1,12 @@
+package com.ze.hackathon.Hackathon2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackathon2018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hackathon2018Application.class, args);
+	}
+}
