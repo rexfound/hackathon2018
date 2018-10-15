ea4ac74bed586a45226e858478293739836533e1
ttsung <terencetsung@gmail.com>
2018-10-15 15:51:47 -0700

    Changed API key

--- src/main/java/com/ze/hackathon/Hackathon2018/Hackathon2018Application.java
+++ src/main/java/com/ze/hackathon/Hackathon2018/Hackathon2018Application.java
@@ -64,7 +64,7 @@ public class Hackathon2018Application {
 				//https://api.openweathermap.org/data/2.5/find?lat=49.2&lon=-122.95&cnt=50&appid=17556070bfb60252ba8bcd48fc9408d5
 				while (true) {
 					ResponseEntity<String> result = restTemplate.exchange(
-							"https://api.openweathermap.org/data/2.5/find?lat=49.2&lon=-122.95&cnt=50&appid={id}", HttpMethod.GET, httpEntity, String.class, "17556070bfb60252ba8bcd48fc9408d5");
+							"https://api.openweathermap.org/data/2.5/find?lat=49.2&lon=-122.95&cnt=50&appid={id}", HttpMethod.GET, httpEntity, String.class, "120681b61a02605e9509860438ffb6bd");
 					parseJSONData(result);
 					TimeUnit.SECONDS.sleep(10);
 				}
